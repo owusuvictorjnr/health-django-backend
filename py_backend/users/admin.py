b884c04eefc5887a7f1613fe8e6b1472512f1b1c
owusuvictorjnr <owusuvictor80@gmail.com>
2025-01-19 21:42:45 +0000

    admin panel

--- py_backend/users/admin.py
+++ py_backend/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
