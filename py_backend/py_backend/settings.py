2cf1a8fa04c724512b1e0eefa167ec3f552d5904
owusuvictorjnr <owusuvictor80@gmail.com>
2025-01-19 21:26:08 +0000

    whitespace

--- py_backend/py_backend/settings.py
+++ py_backend/py_backend/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD':  config('DB_PASSWORD'),
-        "HOST":config('DB_HOST', default='localhost'),
+        "HOST": config('DB_HOST', default='localhost'),
         "PORT": config('DB_PORT', default='5432'),
     }
     # 'default': {
